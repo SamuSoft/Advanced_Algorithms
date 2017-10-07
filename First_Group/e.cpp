7a7d5072963608dd2a1262c028ae278e29ecc24f
サム <samuelph@kth.se>
2017-10-07 19:02:49 +0200

    Basics of E

--- First_Group/e.cpp
+++ First_Group/e.cpp
@@ -2,24 +2,46 @@
 #include <stdlib.h>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+
 
 using namespace std;
 
 vector<int> makeRandVec(int count){
   vector<int> retVal(count);
-  int val = rand()%100;
+  int val = rand()%200;
   for (size_t i = 0; i < count; i++) {
     retVal[i] = val++;
   }
   return retVal;
 }
 
-
+bool findRightAnswer(const vector<int>& Bob, const vector<int>& Alice, const int len){
+  int messageSize = 6;
+  int ref;
+  for (size_t i = 0; i < messageSize; i++) {
+    ref = Bob[rand()%len];
+    cout << ref << endl;
+    if (std::find(Alice.begin(), Alice.end(), ref) != Alice.end()) {
+      return true;
+    }
+  }
+  return false;
+}
 
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
   vector<int> Bob = makeRandVec(100);
   vector<int> Alice = makeRandVec(100);
-  cout << Bob[0] << " "  << Alice[0] << endl;
+  int start = 0;
+  int end = 0;
+  if (Bob[0] > Alice[99] || Bob[99] < Alice[0]) {
+
+  }else(){
+
+  }
+
+  cout << Bob[0] << " "  << Alice[0] << endl << findRightAnswer(Bob,Alice,100);
+
   return 0;
 }
