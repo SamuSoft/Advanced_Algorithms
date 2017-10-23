ffc1bdf4e57ae4d826292cda7f7877f882146dd4
SamuSoft <samuelph@kth.se>
2017-10-23 23:44:44 +0200

    Added climber, now climbs to local max

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <algorithm>
 
+std::vector<std::pair<double,double> > waypoints;
+
 std::vector<std::pair<double,double> > createTravelVector(){
   int len;
   std::cin >> len;
@@ -17,6 +19,37 @@ std::vector<std::pair<double,double> > createTravelVector(){
   return list;
 }
 
+int length(std::vector<int> trip){
+  auto data = waypoints;
+  int retval = 0;
+  for (size_t i = 1; i < trip.size(); i++){
+    retval += (int) sqrt(
+                    pow(data[trip[i-1]].first - data[trip[i]].first,2) +
+                    pow(data[trip[i-1]].second - data[trip[i]].second,2)
+                  );
+  }
+  return retval;
+}
+
+std::vector<int> swapper(std::vector<int> list){
+  std::vector<int>::iterator a = list.begin(); std::vector<int>::iterator b = ++list.begin();
+  int len = length(list);
+  int newlen = len;
+  auto print = [](std::vector<int> l){for(int x : l){std::cout<<l[x]<<" ";} std::cout<<std::endl;};
+  for(int a = 1; a < list.size(); a++) {
+    for(int b = a+1; b < list.size(); b++){
+      std::swap(list[a],list[b]);
+      newlen = length(list);
+      if(newlen < len){
+        a = 0; len = newlen; break;
+      }else {
+        std::swap(list[a],list[b]);
+      }
+    }
+  }
+  return list;
+}
+
 int main(int argc, char const *argv[]) {
   // number of random solution samples
   int count = 50;
@@ -24,22 +57,27 @@ int main(int argc, char const *argv[]) {
 
   srand(time(NULL));
 
-  std::vector<std::pair<double,double> > waypoints = createTravelVector();
-  std::vector<int> list = naive::greedyTour(waypoints);
-  std::pair<std::vector<int>,int> randlist;
+  waypoints = createTravelVector();
+  auto list = naive::greedyTour(waypoints);
+  std::vector<int> randlist;
   std::vector<int> truelist;
 
   int cost = 99999;
+  int lcost;
   for (size_t i = 0; i < count; i++) {
-    randlist = prob::compute(waypoints);
-    if (randlist.second < cost) {
-      truelist = randlist.first;
-      cost = randlist.second;
+    randlist = prob::getRandList(waypoints);
+    lcost = length(randlist);
+    if (lcost < cost) {
+      truelist = randlist;
+      cost = lcost;
     }
   }
 
+  auto reflist = swapper(truelist);
+  std::cout << "Greedy:\tProb:\tSwapped:"<< std::endl;
+  std::cout << length(list) << "\t" << length(truelist) << "\t"<< length(reflist)<< std::endl;
   for (size_t i = 0; i < list.size(); i++) {
-    std::cout << list[i] << " " << truelist[i] << std::endl;
+    std::cout << list[i] << "\t" << truelist[i] << "\t" << reflist[i] << std::endl;
   }
   return 0;
 }
--- Project1/prob.h
+++ Project1/prob.h
@@ -2,49 +2,20 @@
 #ifndef prob_H
 #define prob_H
 
-#include <stdlib.h>
 #include <vector>
-#include <math.h>
-#include <tuple>
 #include <algorithm>
-
 
 //returns a random solution as a pair of solution list and total length
 
 class prob{
   private:
-    static std::vector<std::tuple<int,double,double> >  copyList(const std::vector<std::pair<double,double> >& input){
-      std::vector<std::tuple<int,double,double> > retval(input.size());
-      for (size_t i = 0; i < input.size(); i++) {
-          retval[i] = std::tuple<int,double,double>(i,input[i].first, input[i].second);
-      }
-      return retval;
-    }
-
-    static std::pair<std::vector<int>,int> compTrip(const std::vector<std::tuple<int,double,double> >& trip){
-      int retval = 0;
-      std::vector<int> v(trip.size());
-      v[0] = std::get<0>(trip[0]);
-      for (size_t i = 1; i < trip.size(); i++) {
-        v[i] = std::get<0>(trip[i]);
-        retval += sqrt(
-                        pow(std::get<1>(trip[i-1]) - std::get<1>(trip[i]),2) +
-                        pow(std::get<2>(trip[i-1]) - std::get<2>(trip[i]),2)
-                      );
-      }
-
-      return std::pair<std::vector<int>,int>(v,retval);
-    }
-    static std::pair<std::vector<int>,int> getRandOrd(const std::vector<std::pair<double,double> >& input){
-      std::vector<std::tuple<int,double,double> > copy = copyList(input);
-      std::random_shuffle(++copy.begin(),copy.end());
-      return compTrip(copy);
-    }
-    // Best I can do to create a static class in C++
     prob(){}
   public:
-    static std::pair<std::vector<int>,int> compute(const std::vector<std::pair<double,double> >& input){
-      return getRandOrd(input);
+    static std::vector<int> getRandList(const std::vector<std::pair<double,double> >& input){
+      std::vector<int> retval(input.size());
+      std::iota(retval.begin(), retval.end(),0);
+      std::random_shuffle(++retval.begin(),retval.end());
+      return retval;
     }
 };
 #endif
